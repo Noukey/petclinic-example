06ded9fada98b44850bb20668926253cab53193f
noukey <670908858@qq.com>
2018-02-26 10:53:51 +0800

    init

---
+++ ConfigServer/src/main/java/noukey/springcloud/example/petclinic/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package noukey.springcloud.example.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
